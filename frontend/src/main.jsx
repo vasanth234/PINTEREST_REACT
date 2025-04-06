524183a23af69d421619f6189397051c26cdf405
Vasanth <p.vasanth@accenture.com>
2025-04-07 00:06:13 +0530

    applied routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import homePage from './routes/homepage/homePage';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-
+import AboutPage from './routes/aboutPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import createPage from './routes/createPage/createPage';
+import postPage from './routes/postPage/postPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-     <Routes>
-      <Route  path="/"  element={<homePage/>}  />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<homePage />} />
+        <Route path="/create" element={<createPage />} />
+        
+      
+        <Route path="/pin/:id" element={<postPage/>}>
+         
+          <Route path="details" element={<AppDetails />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  
   </React.StrictMode>
 );
